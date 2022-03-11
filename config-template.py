9fee4afba5dc838cd832c3aa727645200a5b0431
(Stein) Runar Bergheim <rb@avinet.no>
2022-03-11 10:51:39 +0100

    Added config template

--- config-template.py
+++ config-template.py
@@ -1,1 +1,13 @@
+import os 
 APP_BASE_PATH = "/"
+
+# Connection string
+SQLALCHEMY_CONNSTR = "sqlite:///../../db/polirural.sqlite"
+
+# Define path directory for models
+MODEL_PATH = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'models')
+
+# Setup serving of static files, if any
+STATIC_PATH = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'static')
