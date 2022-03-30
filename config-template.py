fa5a9a9d50cbd34acdb207ac37acc2c8920f447e
(Stein) Runar Bergheim <rb@avinet.no>
2022-03-30 23:53:49 +0200

    Fixes

--- config-template.py
+++ config-template.py
@@ -1,13 +1,16 @@
-import os 
+from os.path import dirname, join, abspath
+
+# Base path that the app is served from
 APP_BASE_PATH = "/"
 
 # Connection string
-SQLALCHEMY_CONNSTR = "sqlite:///../../db/polirural.sqlite"
+SQLALCHEMY_CONNSTR = "sqlite:///%s" % (join(dirname(
+    abspath(__file__)), 'db/polirural.sqlite'))
 
 # Define path directory for models
-MODEL_PATH = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), 'models')
+MODEL_PATH = join(dirname(
+    abspath(__file__)), 'models')
 
 # Setup serving of static files, if any
-STATIC_PATH = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), 'static')
+STATIC_PATH = join(dirname(
+    abspath(__file__)), 'static')
