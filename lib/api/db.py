f3e44c40eb6553745c8025674c1fa102e9fe9023
(Stein) Runar Bergheim <rb@avinet.no>
2022-03-30 23:47:37 +0200

    Fixed set password function

--- lib/api/db.py
+++ lib/api/db.py
@@ -38,7 +38,7 @@ class Users(db.Model, CustomModel):
     role = db.Column(JSON, nullable=True)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)    
