6ee82705b7c6b9a406f30c644db21d92b92216a8
(Stein) Runar Bergheim <rb@avinet.no>
2022-03-30 23:58:19 +0200

    Fixes

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import lib.api.pysdutil
 import lib.api.routes
 
 logging.basicConfig(level=logging.DEBUG)
+compress.init_app(server)
 
 # Run as local dev server when executed from command line
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server. 
-    compress.init_app(server)
     server.run(debug=True)
